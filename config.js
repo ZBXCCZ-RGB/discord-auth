3ca37ed37783b5ca40ddacd614f774dcef70c89a
ZBXCCZ-RGB <cfif20042123@gmail.com>
2026-01-31 23:41:40 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+// config.js - НЕ КОММИТЬТЕ ЭТОТ ФАЙЛ В ПУБЛИЧНЫЙ РЕПОЗИТОРИЙ!
+// Этот файл должен быть ТОЛЬКО на вашем локальном компьютере
+
+const SECRET_CONFIG = {
+    CLIENT_SECRET: 'ВАШ_РЕАЛЬНЫЙ_CLIENT_SECRET',
+    BOT_TOKEN: 'ВАШ_РЕАЛЬНЫЙ_BOT_TOKEN',
+    GUILD_ID: 'ВАШ_SERVER_ID',
+    INVITE_CODE: 'ВАШ_INVITE_CODE'
+};
+
+// Для GitHub Pages мы используем proxy-сервер
+const PROXY_URL = 'https://ВАШ-PROXY.herokuapp.com'; // Читайте ниже как создать
